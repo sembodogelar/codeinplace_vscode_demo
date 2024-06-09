c441986039169d19be8154761e6eaaf3ae652829
SembodoGelar <sembodogelar6@gamail.com>
2024-06-09 17:30:30 +0700

    Add third program and change the farenheit value

---
+++ my_second _program.py
@@ -0,0 +1,8 @@
+
+def convert_c_to_f(temp_in_celcius):
+    temp_in_farenheit = temp_in_celcius * 1.8 + 32
+    return temp_in_farenheit
+
+
+temp_in_f = convert_c_to_f(35)
+print(temp_in_f)
---
+++ my_third_program.py
@@ -0,0 +1,6 @@
+def greeting():
+    print("Mosimos")
+    print("Hello")
+    print("Bonjour")
+
+greeting()
